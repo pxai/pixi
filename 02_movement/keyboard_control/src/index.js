26d037bfcb869a282b79fe1bbd8376876414a940
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-08-11 00:26:38 +0200

    ok

--- 02_movement/keyboard_control/src/index.js
+++ 02_movement/keyboard_control/src/index.js
@@ -29,6 +29,7 @@ function init() {
   logo.vx = 1;
   logo.vy = 1;
   state = right;
+  console.log('Game init')
 }
 
 function right() {
@@ -119,6 +120,48 @@ function setup() {
 
     //Start the game loop
     gameLoop();
+}
+
+
+function keyboard(keyCode) {
+    let key = {};
+    key.code = keyCode;
+    key.isDown = false;
+    key.isUp = true;
+    key.press = undefined;
+    key.release = undefined;
+
+    //The `downHandler`
+    key.downHandler = event => {
+        if (event.keyCode === key.code) {
+            if (key.isUp && key.press) key.press();
+            key.isDown = true;
+            key.isUp = false;
+        }
+    event.preventDefault();
+    };
+    //The `upHandler`
+    key.upHandler = event => {
+    if (event.keyCode === key.code) {
+    if (key.isDown && key.release) key.release();
+    key.isDown = false;
+    key.isUp = true;
+    }
+
+    event.preventDefault();
+    };
+
+    //Attach event listeners
+    window.addEventListener(
+    "keydown", key.downHandler.bind(key), false
+    );
+
+    window.addEventListener(
+    "keyup", key.upHandler.bind(key), false
+    );
+
+    //Return the `key` object
+    return key;
 }
 
 function play() {

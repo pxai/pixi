8b856e4ad77e707600bcdf29b8f5f91ace1f0190
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-08-20 09:29:03 +0200

    corrected but TODO

--- 04_sound/play_sound/src/index.js
+++ 04_sound/play_sound/src/index.js
@@ -2,3 +2,18 @@ const PIXI = require("pixi.js");
 
 let app = new PIXI.Application(600, 400, {backgroundColor : 0x1099bb});
 document.body.appendChild(app.view);
+
+const sound = PIXI.sound.Sound.from('assets/sounds/smb_coin.mp3');
+sound.play();
+
+
+PIXI.sound.Sound.from({
+    url: 'assets/sounds/smb_jump-small.mp3',
+    autoPlay: true,
+    complete: function() {
+        console.log('Sound finished');
+    }
+});
+
+PIXI.sound.add('coin', 'assets/sounds/smb_powerup.mp3');
+PIXI.sound.play('coin');

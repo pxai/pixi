9233db7b538ee365895526ea13259ae057caba6d
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-08-23 16:59:26 +0200

    Added gravity sample

---
+++ 02_movement/gravity/src/index.js
@@ -0,0 +1,36 @@
+const PIXI = require("pixi.js");
+
+var app = new PIXI.Application(800, 600, {backgroundColor : 0x1099bb});
+document.body.appendChild(app.view);
+
+// create a new Sprite from an image path
+const logo = PIXI.Sprite.from("assets/images/pixilogo.png");
+
+// center the sprite's anchor point
+logo.anchor.set(0.5);
+
+// move the sprite to the center of the screen
+logo.x = app.screen.width / 2;
+logo.y = app.screen.height / 2;
+
+app.stage.addChild(logo);
+    console.log(logo);
+
+// Acceleation and friction
+logo.vx = logo.vx + logo.accelerationX;
+logo.vy = logo.vy + logo.accelerationY;
+logo.vx = logo.vx * logo.frictionX;
+logo.vy = logo.vx * logo.frictionY;
+
+// Add gravity
+logo.vy = logo.vy + 0.1;
+
+function gameLoop(){
+   //Loop this function 60 times per second
+   requestAnimationFrame(gameLoop);
+   logo.x = logo.x + logo.vx;
+   logo.y = logo.y + logo.vy;
+   app.renderer.render(app.stage);
+}
+
+gameLoop();

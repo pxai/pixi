8df1bdcce41e9487362c59d2bd26ca70acb54602
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-08-27 23:36:10 +0200

    Changed basic

--- 06_gameloop/simple/src/index.js
+++ 06_gameloop/simple/src/index.js
@@ -3,7 +3,7 @@ const PIXI = require("pixi.js");
 var app = new PIXI.Application(400, 300, {backgroundColor : 0x00dead});
 document.body.appendChild(app.view);
 
-var basicText = new PIXI.Text('Hello Pixi mouse click');
+var basicText = new PIXI.Text('Hello Pixi, game loo');
 
 basicText.x = app.screen.width / 4;
 basicText.y = app.screen.height / 3;
